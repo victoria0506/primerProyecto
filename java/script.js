a71486cf039aa8a2137677740a5eebd8cdffab8b
victoria0506 <vicguti527@gmail.com>
2024-04-30 09:10:14 -0600

    Actu java

--- java/script.js
+++ java/script.js
@@ -5,19 +5,24 @@ let input = document.querySelector("#input")
 let boton = document.querySelector("#boton")
 boton.addEventListener("click", function() {
     let mensaje = document.querySelector("#uno")
-    mensaje.innerHTML = "Su mensaje es : " + input.value
-    alert("Gracias po su mensaje")
-
+    if (input.value == "") {
+        alert("No agregaste mensaje")
+    } else {
+        mensaje.innerHTML = "Su mensaje es : " + input.value
+        alert("Gracias por su mensaje")
+    }
 })
-
-
 
 let boton1 = document.querySelector("#boton1")
 boton1.addEventListener("click", function () {
     alert("Gracias por su interes")
     let mens = document.querySelector("#dos")
-    mens.innerHTML = " Otros contacatos ....."
+    mens.innerHTML = " Más proyectos : Github Victoria0506 "
 })
+
+
+
+
 
 
 
