544c0daf6a1e95eb4f26a74659ae67c63a335284
victoria0506 <vicguti527@gmail.com>
2024-05-01 09:12:10 -0600

    documentacion del codigo

--- java/script.js
+++ java/script.js
@@ -1,34 +1,60 @@
 
 
-
+//variale del input
 let input = document.querySelector("#input")
+
+//variable del boton de mensaje
 let boton = document.querySelector("#boton")
+
+//funcion del boton 
 boton.addEventListener("click", function() {
+
+    //variable de la h4 donde se va a imprimir el mensaje
     let mensaje = document.querySelector("#uno")
+
+    //validacion de si hay o no hay mensaje para enviar
     if (input.value == "") {
         alert("No agregaste mensaje")
-    } else {
+    }else{
         mensaje.innerHTML = "Su mensaje es : " + input.value
         alert("Gracias por su mensaje")
     }
 })
 
-let boton1 = document.querySelector("#boton1")
-boton1.addEventListener("click", function () {
+//variable del boton de más información 
+let btninfo = document.querySelector("#boton1")
+
+//funcion de el btn más información 
+btninfo.addEventListener("click", function () {
     alert("Gracias por su interes")
+
+    //variable del h4 para la informacion del perfil de Github
     let mens = document.querySelector("#dos")
     mens.innerHTML = " Más proyectos : Github Victoria0506 "
 })
 
+//Variable del btn del corazón 
+let btncora = document.querySelector("#cora")
 
-let cora = document.querySelector("#cora")
-cora.addEventListener("click", function() {
+//funcion del btn de corazón
+btncora.addEventListener("click", function() {
+
+    //variable de la etiqueta p donde se va a imprimir los me gusta
     let conta = document.querySelector("#con")
+
+    //el innerHTML++ es para que se valla imcrementando de uno en uno
     conta.innerHTML++
 })
 
+//Variable del btn para eliminar todo las informaciones
 let eliminar = document.querySelector("#eli")
+
+//funcion del btn de eliminar
 eliminar.addEventListener("click", function() {
+    //variable de la etiqueta p de likes 
+    //el metodo remove() es para remover lo que agregamos 
+    // aqui removemos lo agregado en las etiquetas p de contador
+    //y en las h4 de mensaje y informacion
     let eli = document.querySelector("#con")
     eli.remove()
     let eli1 = document.querySelector("#uno")
@@ -36,6 +62,8 @@ eliminar.addEventListener("click", function() {
     let eli2 = document.querySelector("#dos")
     eli2.remove()
 })
+
+
 
 
 
